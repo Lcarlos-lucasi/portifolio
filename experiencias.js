fd1fec3c537ea9d5aee9bd290a1dfac61701c3c4
Lcarlos-lucasi <lcarlos.lucasi@gmail.com>
2022-09-27 18:23:44 -0300

    primeira comite

---
+++ experiencias.js
@@ -0,0 +1,9 @@
+const engExperiencias = {funcao:'Engenheiro de Processo',
+empresa:'Companhia Industrial de Vidros',
+dataInicil:01/03/2022, 
+dataFinal:10/07/1998,
+local: Recife-PE,
+atividades:"- Projetos de moldes e equipamento; - Desenvolvimento de novos produtos; - Alteração e atualização de projetos; - Acompanhamento da produção; -	Coordenação das larchas (Fornos de alívio de tensões e tratamentos superficiais); -	Instrutor de treinamentos; - Auditor interno da qualidade (Normas ISO 9000); -	Acompanhamento das implantações de novas tecnologias e máquinas no setor; -	Melhorias e desenvolvimento dos processos;
+-	Recepção de técnicos estrangeiros.
+"
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+function engenheiro(){
+    window.location.href="engenheiro.html";
+}
+
+function home(){
+    window.location.href="index.html";
+}
